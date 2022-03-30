0c651f4750e693b3bf40b6696045e21d1859c880
MathiasIZZ <mathias.guiguen@gmail.com>
2022-03-30 17:30:03 +0200

    near end

--- back/src/config/mongo.config.ts
+++ back/src/config/mongo.config.ts
@@ -3,11 +3,6 @@ import mongoose from "mongoose";
 export const setMongoConnection = () => {
     const mongoUrl = `mongodb://${process.env["MONGO_HOST"] || "localhost"}:27017`;
 
-    const MONGO_USERNAME=process.env.MONGO_USERNAME;
-    const MONGO_PASSWORD=process.env.MONGO_PASSWORD;
-    const MONGO_URL=process.env.MONGO_URL;
-
-
     console.log(mongoUrl)
     mongoose.connect(mongoUrl, {
         useCreateIndex: true,
